02f04ee644988d112388781882857a36752b8500
RusyaKhalmatov <rusya.khalmatov@gmail.com>
2020-04-23 23:35:37 +0500

    third commit

---
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Leads extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Leads component</h1>
+            </div>
+        )
+    }
+}
+
+export default Leads
